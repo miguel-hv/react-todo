6870b62311ab4bdc502dde78ca73602ebdd2c594
mhdelavega <huertadelavegamiguel@gmail.com>
2022-01-14 17:20:54 +0100

    FEAT:add todo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import { useState } from 'react';
 import './App.scss';
 
-const initialState = [
+const todoListInitialState = [
   {id: 1,
   todo: 'esta es mi primera tarea',
   priority: 5},
@@ -12,33 +12,68 @@ const initialState = [
   priority: 5},
   ];
 
+// const todoInitialState = {
+//   id:'',
+//   todo:'hola',
+//   priority:'',
+// }
+
 function App() {
 
-  const [ todos, setTodos ] = useState(initialState);
+  const [ todoList, setTodoList ] = useState(todoListInitialState);
+  const [ todo, setTodo ] = useState('');
+  const [ priority, setPriority ] = useState('');
 
-  const addTodo = (task) => {
-    console.log('tarea a;adida'+task);
+  // const addTodo = (task) => {
+  //   console.log('tarea a;adida'+task);
+  //   setTodos([...todos, task]);
+  //   console.log(todos);
+  // }
+  const handleFormSubmit = ev => {
+    ev.preventDefault();
+    setTodoList([...todoList, {
+      id: 0,
+      todo: todo,
+      priority: priority,
+    }]);
   }
+
 
   return (
     <>
-      <TextareaAutosize
-        id="addTodo"
-        name="addTodo"
-        aria-label="empty textarea"
-        placeholder="Tarea"
-        minRows={3}
-        maxRows={6}
-        style={{ width: 200 }}
-      />
-      <Button variant='contained' onClick={addTodo}>Añadir tarea</Button>
-      <div>
-        {todos.map(e => (
-          <li key={e.id}>
-            task: {e.todo}
-          </li>
-        ))}
-      </div>
+      <form onSubmit={handleFormSubmit}>
+        {/* <TextareaAutosize
+          id="addTodo"
+          name="addTodo"
+          aria-label="empty textarea"
+          placeholder="Tarea"
+          minRows={3}
+          maxRows={6}
+          style={{ width: 200 }}
+        />
+        <Button variant='contained' onClick={addTodo}>Añadir tarea</Button> */}
+        <input 
+          type="text"
+          placeholder="task to do"
+          name="todo"
+          value={todo.todo}
+          onChange={e => setTodo(e.target.value)}
+        />
+        <input    //cambiar a selector
+          type="text"
+          name="priority"
+          value={todo.priority}
+          onChange={e => setPriority(e.target.value)}
+        />
+        <button type="submit">Añadir</button>
+      </form>
+        <div>
+          {todoList.map(e => (
+            <li key={e.id}>
+              task: {e.todo}
+            </li>
+          ))}
+        </div>
     </>
   );
 }
